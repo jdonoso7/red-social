142625ad692c3ea2627979b7d6e0ed7cf85c8d3c
Jesus <jesus.donoso@sngular.com>
2023-12-07 18:06:09 +0100

    Add User List

--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -1,3 +1,32 @@
 <template>
-    hola
+  <div>
+    <h2>Listado de Usuarios</h2>
+    <ul>
+      <li v-for="user in userList" :key="user.id">
+          {{ user.name }}
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import { users } from '../api';
+export default {
+  setup() {
+    const userList = ref([]);
+
+    const fetchUserList = async () => {
+      userList.value= await users.getAll()
+    };
+
+    onMounted(() => {
+      fetchUserList();
+    });
+
+    return {
+      userList,
+    };
+  },
+};
+</script>
