a662522719e21fa445cc26c0c058d031a01e2999
Jesus <jesus.donoso@sngular.com>
2023-12-07 18:05:59 +0100

    get all User api

--- src/api/users.js
+++ src/api/users.js
@@ -4,7 +4,19 @@ export default (_client) => {
     return data;
   };
 
+  const getUserDetails = async (id) => {
+    const { data } = await _client.get(`users/${id}`);
+    return data;
+  };
+
+  const getUserAlbums = async (id) => {
+    const { data } = await _client.get(`users/${id}/albums`);
+    return data;
+  };
+
   return {
     getAll: getUsers,
+    getDetails: getUserDetails,
+    getAlbums: getUserAlbums
   };
 };
